7d7c92d53b58e3181b4713060f38fbcd48b7869f
Giovanni <indirizzoemail@mail.com>
2021-05-18 08:09:40 +0200

    use full path for battery

--- blocks.h
+++ blocks.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
 	{ "ip: ", 		"ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1", 			120, 	0 },
 	{ "vpn: ", 		"pgrep -x 'openvpn' > /dev/null && echo yes || echo no",					10, 	0 },
 	{ "docker: ", 		"systemctl is-active docker > /dev/null && echo running || echo stopped",					10, 	0 },
-	{ "battery: ", 	"battery | tr -d '\n' && echo %",											60, 	0 },
+	{ "battery: ", 	".local/bin/battery | tr -d '\n' && echo %",											60, 	0 },
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
