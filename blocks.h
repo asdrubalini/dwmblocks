8e3b561555c631a409bd6e7291ed461459d652e9
Giovanni <indirizzoemail@mail.com>
2021-05-20 10:27:38 +0200

    fix blocks config

--- blocks.h
+++ blocks.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/* Icon */  /* Command */ 													/* Update Interval */ /* Update Signal */
 	{ "", 			"date '+%H:%M %d-%m-%y'", 										  			10, 	0 },
 	{ "ip: ", 		"ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1", 			120, 	0 },
-	{ "ram: ", 	".local/bin/used-ram-percentage | tr -d '\n' && echo %",											10, 	0 },
+	{ "ram: ", 	".local/bin/used-ram-percentage | tr -d '\n' && echo %",											60, 	0 },
 	{ "vpn: ", 		"pgrep -x 'openvpn' > /dev/null && echo yes || echo no",					10, 	0 },
 	{ "docker: ", 		"systemctl is-active docker > /dev/null && echo running || echo stopped",					10, 	0 },
 	{ "battery: ", 	".local/bin/battery | tr -d '\n' && echo %",											60, 	0 },
