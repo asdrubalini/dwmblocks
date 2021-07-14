e0c5a389436bf68828f71b67c9f5e3337df8d4ca
Giovanni <indirizzoemail@mail.com>
2021-07-15 00:02:54 +0200

    use get-ip script

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/* Icon */  /* Command */ 													/* Update Interval */ /* Update Signal */
 	{ "", 			"date '+%H:%M %d-%m-%y'", 										  			10, 	0 },
-	{ "ip: ", 		"ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1", 			120, 	0 },
-	{ "ram: ", 	".local/bin/used-ram-percentage | tr -d '\n' && echo %",											60, 	0 },
+	{ "ip: ", 		"$HOME/.local/bin/get-ip", 			120, 	0 },
+	{ "ram: ", 	    "$HOME/.local/bin/used-ram-percentage | tr -d '\n' && echo %",											60, 	0 },
 	{ "vpn: ", 		"pgrep -x 'openvpn' > /dev/null && echo yes || echo no",					10, 	0 },
-	{ "docker: ", 		"systemctl is-active docker > /dev/null && echo running || echo stopped",					10, 	0 },
-	{ "battery: ", 	".local/bin/battery | tr -d '\n' && echo %",											60, 	0 },
+	{ "docker: ", 	"systemctl is-active docker > /dev/null && echo running || echo stopped",					10, 	0 },
+	{ "battery: ", 	"$HOME/.local/bin/battery | tr -d '\n' && echo %",											60, 	0 },
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
